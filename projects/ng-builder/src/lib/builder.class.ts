d43980443a45e3f0f0c8210fde86eb0a857aa116
Soeren Balke <balke@id-stiftung.de>
2018-08-20 12:34:28 +0200

    bugfix: init values for builder class

--- projects/ng-builder/src/lib/builder.class.ts
+++ projects/ng-builder/src/lib/builder.class.ts
@@ -1,10 +1,15 @@
-import { Input, Output, EventEmitter } from '@angular/core';
+import { Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 
-export class Builder {
-  @Input() data: any = {};
-  @Input() edit = false;
+export class Builder implements OnInit {
+  @Input() data: any;
+  @Input() edit: boolean;
   @Output() saveEdit = new EventEmitter();
 
   constructor() {}
+
+  ngOnInit() {
+    this.data  = {};
+    this.edit = false;
+  }
 }
