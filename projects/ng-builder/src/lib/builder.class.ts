869d4e34c654460ba9cea45f7aebd2e760056ed1
Sören Balke <balkesoeren@googlemail.com>
2020-02-07 17:13:02 +0100

    update angular to 9

--- projects/ng-builder/src/lib/builder.class.ts
+++ projects/ng-builder/src/lib/builder.class.ts
@@ -1,6 +1,7 @@
-import { Input, Output, EventEmitter } from '@angular/core';
+import { Input, Output, EventEmitter, Directive } from '@angular/core';
 
 
+@Directive()
 export class Builder {
   @Input() data: any;
   @Input() edit: boolean;
