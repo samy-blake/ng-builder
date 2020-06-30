069cc1c859f7e1abfa092c271431d910e2a57469
Sören Balke <balkesoeren@googlemail.com>
2020-06-30 12:55:02 +0200

    update to angular 10

--- projects/ng-builder/src/lib/builder.class.ts
+++ projects/ng-builder/src/lib/builder.class.ts
@@ -1,5 +1,6 @@
-import { Input, Output, EventEmitter } from '@angular/core';
+import { Input, Output, EventEmitter, Directive } from '@angular/core';
 
+@Directive()
 export class Builder {
   @Input() data: any;
   @Input() edit: boolean;
